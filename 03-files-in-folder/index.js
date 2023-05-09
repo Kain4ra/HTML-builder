8a6dcec82e170de3b4e2f331ede3dd6b2a59f083
KainAra <kainara.idwsi@gmail.com>
2023-05-09 18:37:29 +0300

    fix: bug with file without extension

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,8 +8,8 @@ fs.readdir(folderPath, {withFileTypes: true}, (err, file) => {
   for (let item of file) {
     if (item.isFile()) {
       const filePath = path.join(folderPath, item.name);
-      const fileName = item.name.split('.')[0];
       const fileExtend = path.extname(filePath).slice(1);
+      const fileName = fileExtend !== '' ? item.name.replace(`.${fileExtend}`, '') : item.name;
       fs.stat(filePath, (err, file) => {
         if (err) console.log(err);
         console.log(`${fileName} - ${fileExtend} - ${(file.size / 1024)}kb`);
